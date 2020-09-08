e571bdb13bea3e5da77c3096d6d6f7865e0082ec
Kalenda M. Kabukala <kalendamoise@gmail.com>
2020-09-08 15:57:22 -0400

    move sample_files folder into the tests directory, implements more tests.

--- tests/word_search_tests.py
+++ tests/word_search_tests.py
@@ -1,6 +1,31 @@
+import os
+import sys
 from word_search import search_by_simple_string_matching
+import pytest
+
+FILE_DIR_NAME = '/sample_files/' 
+DIRECTORY = os.getcwd() + FILE_DIR_NAME
 
 def test_search_by_simple_string_matching():
     search_term = 'military'
-    file_path = 'sample_files/french_armed_forces.txt'
+    file_path = DIRECTORY + 'french_armed_forces.txt'
     assert search_by_simple_string_matching(search_term, file_path) == 4
+
+
+def test_search_by_simple_string_matching_no_match():
+    search_term = 'Kalenda'
+    file_path = DIRECTORY + 'french_armed_forces.txt'
+    assert search_by_simple_string_matching(search_term, file_path) == 0
+
+
+def test_search_by_simple_string_matching_hitchhikers():
+    search_term = "Hitchhiker's"
+    file_path = DIRECTORY + 'hitchhikers.txt'
+    assert search_by_simple_string_matching(search_term, file_path) == 5    
+
+    
+def test_search_by_simple_string_matching_file_not_found_error():
+    search_term = "Hitchhiker's"
+    file_path = DIRECTORY + 'made_up_file.txt'
+    with pytest.raises(FileNotFoundError) as e:
+        search_by_simple_string_matching(search_term, file_path)
--- word_search.py
+++ word_search.py
@@ -2,15 +2,15 @@ import pytest
 
 
 
-
-
 def search_by_simple_string_matching(search_term, file_path):
     count = 0
     with open(file_path) as file:
         for line in file:
             words = line.split(' ')
             for word in words:
-                if word == search_term:
+                if word.lower() == search_term.lower():
                     count += 1
 
     return count
+
+
