b75f10466f0565d49a58f826b7e50d445f1b6813
Kalenda M. Kabukala <kalendamoise@gmail.com>
2020-09-08 16:57:04 -0400

    implement search by regular expression

--- tests/word_search_tests.py
+++ tests/word_search_tests.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from word_search import search_by_simple_string_matching
+from word_search import search_by_regular_expression_matching
 import pytest
 
 FILE_DIR_NAME = '/sample_files/' 
@@ -27,5 +28,30 @@ def test_search_by_simple_string_matching_hitchhikers():
 def test_search_by_simple_string_matching_file_not_found_error():
     search_term = "Hitchhiker's"
     file_path = DIRECTORY + 'made_up_file.txt'
-    with pytest.raises(FileNotFoundError) as e:
+    with pytest.raises(FileNotFoundError):
         search_by_simple_string_matching(search_term, file_path)
+
+
+def test_search_by_re_matching():
+    search_term = 'military'
+    file_path = DIRECTORY + 'french_armed_forces.txt'
+    assert search_by_regular_expression_matching(search_term, file_path) == 4
+
+
+def test_search_by_re_matching_no_match():
+    search_term = 'Kalenda'
+    file_path = DIRECTORY + 'french_armed_forces.txt'
+    assert search_by_regular_expression_matching(search_term, file_path) == 0
+
+
+def test_search_by_re_matching_hitchhikers():
+    search_term = "Hitchhiker's"
+    file_path = DIRECTORY + 'hitchhikers.txt'
+    assert search_by_regular_expression_matching(search_term, file_path) == 6    
+
+    
+def test_search_by_re_matching_file_not_found_error():
+    search_term = "Hitchhiker's"
+    file_path = DIRECTORY + 'made_up_file.txt'
+    with pytest.raises(FileNotFoundError):
+        search_by_regular_expression_matching(search_term, file_path)
--- word_search.py
+++ word_search.py
@@ -1,4 +1,5 @@
 import pytest
+import re
 
 
 
@@ -6,11 +7,24 @@ def search_by_simple_string_matching(search_term, file_path):
     count = 0
     with open(file_path) as file:
         for line in file:
-            words = line.split(' ')
-            for word in words:
+            for word in line.split():
                 if word.lower() == search_term.lower():
                     count += 1
 
     return count
 
 
+def search_by_regular_expression_matching(search_term, file_path):
+    count = 0
+    search_val = f"(?=({search_term.lower()}))"
+    with open(file_path) as file:
+        for line in file:
+            # re.finditer(,) return an iterable with stating index of each match. 
+            # the length of the returned array represent the number of matched word
+            count += len([x for x in re.finditer(search_val, line.lower())])
+
+    return count
+
+
+def search_by_index(search_term, file_path):
+    pass    
