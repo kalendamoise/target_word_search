6ec878eaa2e354d1b70129f177fc254ff5f1a27c
Kalenda M. Kabukala <kalendamoise@gmail.com>
2020-09-10 18:52:19 -0400

    implemnt print by relevance using a priority queue.

--- word_search.py
+++ word_search.py
@@ -2,16 +2,16 @@ import pytest
 import re
 import timeit
 import os
+from collections import OrderedDict
+import queue
 
 
 def search_by_simple_string_matching(search_term, file_path):
     count = 0
-    # search_term = re.sub(r'\W+', '',search_term.lower())
     search_term = search_term.lower()
     with open(file_path) as file:
         for line in file:
             for word in line.strip().split():
-                # temp = re.sub(r'\W+', '',word.lower())
                 temp = word.lower()
                 if temp == search_term:
                     count += 1
@@ -21,35 +21,53 @@ def search_by_simple_string_matching(search_term, file_path):
 
 def search_by_regular_expression_matching(search_term, file_path):
     count = 0
-    search_val = f"(?=({search_term.lower()}))"
+    search_term = search_term.lower()
+    pattern = re.compile(search_term)
     with open(file_path) as file:
         for line in file:
-            # re.finditer(,) return an iterable with stating index of each match. 
-            # the length of the returned array represent the number of matched word
-            count += len([x for x in re.finditer(search_val, line.lower())])
+            for word in line.lower().split():
+                if pattern.match(word):
+                    count += 1
 
     return count
 
 
 def build_dict(file_path):
-    words_dict = {}
+    words_dict = dict()
     with open(file_path) as file:
         for line in file:
-            for word in line.strip().split():
-                #temp = re.sub(r'\W+', '', word.lower())
+            for word in line.split():
                 temp = word.lower()
                 if temp in words_dict:
                     words_dict[temp] += 1    
                 else:
                     words_dict[temp] = 1  
 
-    return words_dict
+    return OrderedDict(words_dict)
 
 def search_by_index(search_term, words_dict):
-    # search_term = re.sub(r'\W+', '',search_term.lower())
-    search_term = search_term.lower()    
-    return words_dict[search_term]
+    search_term = search_term.lower()
+    if search_term in words_dict:  
+        return words_dict[search_term]
+    return 0
 
+
+def print_results(caller_details, french_armed_forces, hitchhikers, warp_drive, elapse_time):
+    
+    my_queue = queue.PriorityQueue(3)
+    my_queue.put(french_armed_forces)
+    my_queue.put(hitchhikers)
+    my_queue.put(warp_drive)
+
+    print(f'{caller_details} Search results: ')
+    
+    while not my_queue.empty():
+        val = my_queue.get()
+        print(f'\t{val[1]} - {val[0] * -1 } matches')
+            
+            
+    print(f'Elapsed time: {elapse_time}')
+    print("")
 
 def find_using_string_match(search_term, base_dir, file1, file2, file3):
     start_time = timeit.default_timer()
@@ -62,13 +80,7 @@ def find_using_string_match(search_term, base_dir, file1, file2, file3):
     stop_time = timeit.default_timer()
     elapse_time = stop_time - start_time
     
-    print('String Matching Search results: ')
-    print(f'\t{file1} - {french_armed_forces} matches')
-    print(f'\t{file2} - {hitchhikers} matches')
-    print(f'\t{file3} - {warp_drive} matches')
-    #print(f'\t{file4} - {dante} matches')
-    print(f'Elapsed time: {elapse_time}')
-    print("")
+    print_results("String Matching", (french_armed_forces * -1, file1) , (hitchhikers * -1, file2), (warp_drive * -1, file3), elapse_time)
 
 
 def find_by_regex(search_term, base_dir, file1, file2, file3):
@@ -82,13 +94,7 @@ def find_by_regex(search_term, base_dir, file1, file2, file3):
     stop_time = timeit.default_timer()
     elapse_time = stop_time - start_time
     
-    print('Find by Regex Search results: ')
-    print(f'\t{file1} - {french_armed_forces} matches')
-    print(f'\t{file2} - {hitchhikers} matches')
-    print(f'\t{file3} - {warp_drive} matches')
-    #print(f'\t{file4} - {dante} matches')
-    print(f'Elapsed time: {elapse_time}')
-    print("")
+    print_results("Regex Matching", (french_armed_forces * -1, file1) , (hitchhikers * -1, file2), (warp_drive * -1, file3), elapse_time)
 
 
 def find_by_index(search_term, base_dir, file1, file2, file3):
@@ -107,13 +113,7 @@ def find_by_index(search_term, base_dir, file1, file2, file3):
     stop_time = timeit.default_timer()
     elapse_time = stop_time - start_time
     
-    print('Find By Index Search results : ')
-    print(f'\t{file1} - {french_armed_forces} matches')
-    print(f'\t{file2} - {hitchhikers} matches')
-    print(f'\t{file3} - {warp_drive} matches')
-    #print(f'\t{file4} - {dante} matches')
-    print(f'Elapsed time: {elapse_time}')
-    print("")
+    print_results("Index Matching", (french_armed_forces * -1, file1) , (hitchhikers * -1 , file2), (warp_drive * -1, file3), elapse_time)
 
 
 def main():
@@ -123,20 +123,12 @@ def main():
     file2 = 'hitchhikers.txt'
     file3 = 'warp_drive.txt'
     #file4 = 'dante.txt'
+    print("")
 
     find_by_index(search_term, base_dir, file1, file2, file3)
     find_using_string_match(search_term, base_dir, file1, file2, file3)
     find_by_regex(search_term, base_dir, file1, file2, file3)
 
-
-
-
-
-    
-
-
-    
-    
     
 
 if __name__ == '__main__':
